4f2d90e77e14cd44a647c1202096359b1f5be413
Harry Perez <harryy1242@gmail.com>
2024-09-23 15:28:36 +0200

    chore: refactor implicit retun jest mocks

--- src/modules/health/health.service.spec.ts
+++ src/modules/health/health.service.spec.ts
@@ -4,12 +4,11 @@ import { PrismaClient } from '@prisma/client'
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended'
 import { prisma } from '../../server/prisma/client';
 
-jest.mock('../../server/prisma/client', () => {
-    return {
-        __esModule: true,
-        prisma: mockDeep<PrismaClient>(),
-    };
-});
+jest.mock('../../server/prisma/client', () => ({
+    __esModule: true,
+    prisma: mockDeep<PrismaClient>(),
+}));
+
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
 
 describe('HealthService', () => {
@@ -32,23 +31,23 @@ describe('HealthService', () => {
     });
 
     it('should return "OK" for health status', () => {
-        const spy = jest.spyOn(healthService, 'getHealth').mockReturnValue({status: 'OK'});
+        const spy = jest.spyOn(healthService, 'getHealth').mockReturnValue({ status: 'OK' });
         const response = healthService.getHealth();
         expect(spy).toHaveBeenCalledTimes(1);
-        expect(response).toEqual({status: 'OK'});
+        expect(response).toEqual({ status: 'OK' });
     });
 
     it('should return health status as OK when database is healthy', async () => {
         prismaMock.$executeRaw.mockResolvedValueOnce(1);
         const response = await healthService.getDatabaseHealth();
         expect(prismaMock.$executeRaw).toHaveBeenCalledTimes(1);
-        expect(response).toEqual({status: 'OK'});
+        expect(response).toEqual({ status: 'OK' });
     });
 
     it('should return health status as FAIL when database is down', async () => {
         prismaMock.$executeRaw.mockRejectedValueOnce(new Error('DB error'));
         const response = await healthService.getDatabaseHealth();
         expect(prismaMock.$executeRaw).toHaveBeenCalledTimes(1);
-        expect(response).toEqual({status: 'FAIL'});
+        expect(response).toEqual({ status: 'FAIL' });
     });
 });
